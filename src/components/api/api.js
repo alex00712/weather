5b2f26af64b424cd409aa723f82da41275af96b4
sweety <alex@mail.ru>
2020-08-23 21:02:35 +0300

    v1.3

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 class API {
     constructor(){
         this.language = 'ru'
-        this.api_key = '54ab163ce761aff8d66311a31fa2ce25'
+        this.api_key = 'your_api_key'
     }
     changeLanguage(language){
         this.language = language
@@ -21,9 +21,3 @@ class API {
 }
 
 export default new API();
-
-// latitude: 55.5808104
-// longitude: 37.6631305
-// 54ab163ce761aff8d66311a31fa2ce25
-
-// 439d4b804bc8187953eb36d2a8c26a02
